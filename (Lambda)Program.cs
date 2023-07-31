c5c3ecba41ccb8a8e639ff960b3a318279555103
xandelm <alexandrej.lima.mendes@gmail.com>
2023-07-31 07:40:28 -0300

    First commit

---
+++ (Lambda)Program.cs
@@ -0,0 +1,8 @@
+﻿var input1 = Console.ReadLine();
+var input2 = Console.ReadLine();
+
+int num1 = System.Convert.ToInt32(input1);
+int num2 = System.Convert.ToInt32(input2);
+
+(num1,num2) = (num2,num1);
+Console.WriteLine(num1 + ", " + num2);
